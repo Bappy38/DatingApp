55b1461545ff83137498c7e2a22224d2b1f03ee8
Bappy38 <iqbal.bappy.13@gmail.com>
2021-11-19 22:21:23 +0600

    End of section 12

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { NgxSpinnerModule } from "ngx-spinner";
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
@@ -25,6 +25,8 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { TextInputComponent } from './_cellEditor/text-input/text-input.component';
+import { DateInputComponent } from './_cellEditor/date-input/date-input.component';
 
 @NgModule({
   declarations: [
@@ -41,7 +43,9 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     ServerErrorComponent,
     MemberCardComponent,
     MemberEditComponent,
-    PhotoEditorComponent
+    PhotoEditorComponent,
+    TextInputComponent,
+    DateInputComponent
   ],
   imports: [
     BrowserModule,
@@ -49,6 +53,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule,
     NgxSpinnerModule
   ],
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -9,20 +11,51 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  registerForm: FormGroup;
+  maxDate: Date;
+  validationErrors: string[] = [];
 
   constructor(private accountService: AccountService,
-              private toastr: ToastrService) { }
+              private toastr: ToastrService,
+              private formBuilder : FormBuilder,
+              private router : Router) { }
 
   ngOnInit(): void {
+    this.initForm();
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
+
+  initForm() {
+    this.registerForm = this.formBuilder.group({
+      gender: [ 'male' ],
+      username: [ '', [Validators.required] ],
+      knownAs: [ '', [Validators.required] ],
+      dateOfBirth: [ '', [Validators.required] ],
+      city: [ '', [Validators.required] ],
+      country: [ '', [Validators.required] ],
+      password: [ '', [Validators.required, Validators.minLength(4), Validators.maxLength(8)] ],
+      confirmPassword: [ '', [Validators.required, this.matchPassword('password') ] ]
+    });
+
+    this.registerForm.controls.password.valueChanges.subscribe(() => {
+      this.registerForm.controls.confirmPassword.updateValueAndValidity();
+    });
+  }
+
+  matchPassword(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control?.value === control?.parent?.controls[matchTo].value 
+        ? null : {isMatching: true}; /** Here, control.value -> confirmPassword & control.parent.controls[matchTo].value -> Password **/
+    }
   }
 
   register() {
-    this.accountService.register(this.model).subscribe(response => {
-      console.log(response);
+    this.accountService.register(this.registerForm.value).subscribe(response => {
+      this.router.navigateByUrl('/members');
       this.cancel();
     }, error => {
-      console.error(error);
+      this.validationErrors = error;
     });
   }
 
