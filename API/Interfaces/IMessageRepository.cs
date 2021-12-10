6fad058ccd28e1ff8589afe38407b4ab55c894de
Bappy38 <iqbal.bappy.13@gmail.com>
2021-12-10 17:15:45 +0600

    End of section 15

---
+++ API/Interfaces/IMessageRepository.cs
@@ -0,0 +1,20 @@
+﻿using API.DTOs;
+using API.Helpers;
+using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Interfaces
+{
+    public interface IMessageRepository
+    {
+        void AddMessage(Message message);
+        void DeleteMessage(Message message);
+        Task<Message> GetMessage(int id);
+        Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams);
+        Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername);
+        Task<bool> SaveAllAsync();
+    }
+}
